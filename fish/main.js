3745b58af96ce4d1923b478e2befaf9b7ed724f5
Lamquocviet <lamquocviet2005.nvt@gmail.com>
2023-12-05 20:22:54 +0700

    fish

---
+++ fish/main.js
@@ -0,0 +1,7 @@
+document.querySelector('#button').addEventListener("click", (e)=>{
+    document.querySelector('body').style.backgroundImage = 'url(./imgaes/images.jpg)';
+    document.querySelector('body').style.backgroundImage = ' url(./imgaes/chia-se-73-hinh-nen-nuoc-dep-tinh-khiet-vo-cung-chan-thuc_35.jpg)';
+    document.querySelector('body').style.backgroundImage = 'url(./imgaes/pngtree-beautiful-illustration-of-seabed-water-pattern-blue-image_2140154.jpg)';
+    
+   
+});
